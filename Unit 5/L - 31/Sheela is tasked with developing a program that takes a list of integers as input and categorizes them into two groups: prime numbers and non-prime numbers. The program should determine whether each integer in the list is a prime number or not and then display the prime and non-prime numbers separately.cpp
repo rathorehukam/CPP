8cebdf68443f1b7eed2174b57b131f67af00adf3
Hukam <115365309+rathorehukam@users.noreply.github.com>
2023-10-30 12:09:54 +0530

    Update Sheela is tasked with developing a program that takes a list of integers as input and categorizes them into two groups: prime numbers and non-prime numbers. The program should determine whether each integer in the list is a prime number or not and then display the prime and non-prime numbers separately.cpp

--- Unit 5/L - 31/Sheela is tasked with developing a program that takes a list of integers as input and categorizes them into two groups: prime numbers and non-prime numbers. The program should determine whether each integer in the list is a prime number or not and then display the prime and non-prime numbers separately.cpp
+++ Unit 5/L - 31/Sheela is tasked with developing a program that takes a list of integers as input and categorizes them into two groups: prime numbers and non-prime numbers. The program should determine whether each integer in the list is a prime number or not and then display the prime and non-prime numbers separately.cpp
@@ -8,77 +8,72 @@
 
 
 
+
+
+
 #include <iostream>
-    #include <vector>
-    using namespace std;
+#include <cmath>
 
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    if (num == 2 || num == 3) return true;
+    if (num % 2 == 0) return false;
 
-    bool checkprime(int a){
-
-        if(a<2){
+    for (int i = 3; i <= std::sqrt(num); i += 2) {
+        if (num % i == 0) {
             return false;
         }
-        for (int i = 2; i * i <= a; ++i) {
-            if (a % i == 0) {
-                return false;
-                }
-                }
-                return true;
+    }
+    return true;
+}
 
+int main() {
+    int N;
+    std::cin >> N;
+
+    int* numbers = new int[N];
+    int* primes = new int[N];
+    int* nonPrimes = new int[N];
+    int primeCount = 0;
+    int nonPrimeCount = 0;
+
+    for (int i = 0; i < N; i++) {
+        std::cin >> numbers[i];
+        if (isPrime(numbers[i])) {
+            primes[primeCount] = numbers[i];
+            primeCount++;
+        } else {
+            nonPrimes[nonPrimeCount] = numbers[i];
+            nonPrimeCount++;
+        }
     }
 
-
-    int main(){
-
-        int wl=-1;
-    if(wl==0){
-        cout<<"new delete";
-    }   
-
-        int n;
-        cin>>n;
-
-        int arr[n];
-
-        for(int i=0;i<n;i++){
-            cin>>arr[i];
+    // Display the prime numbers
+    if (primeCount > 0) {
+        std::cout << "Prime numbers: ";
+        for (int i = 0; i < primeCount; i++) {
+            std::cout << primes[i] << " ";
         }
-
-        vector<int> prime;
-        vector<int> non_prime;
-
-        for(int i=0;i<n;i++){
-            if (checkprime(arr[i])){
-                prime.push_back(arr[i]);
-            }
-            else{
-                non_prime.push_back(arr[i]);
-            }
-        }
-
-        if(prime.empty()){
-            cout<<"No prime numbers found.";
-        }
-
-        else{
-        cout<<"Prime numbers: ";
-        for(int i:prime){
-            cout<<i<<" ";
-        }
-        }
-
-        cout<<endl;
-
-        if(non_prime.empty()){
-            cout<<"No non-prime numbers found.";
-        }
-
-        else{
-        cout<<"Non-prime numbers: ";
-        for(int i:non_prime){
-            cout<<i<<" ";
-        }
-        }
-
-        return 0;
+        std::cout << std::endl;
+    } else {
+        std::cout << "No prime numbers found." << std::endl;
     }
+
+    // Display the non-prime numbers
+    if (nonPrimeCount > 0) {
+        std::cout << "Non-prime numbers: ";
+        for (int i = 0; i < nonPrimeCount; i++) {
+            std::cout << nonPrimes[i] << " ";
+        }
+        std::cout << std::endl;
+    } else {
+        std::cout << "No non-prime numbers found.";
+    }
+
+    // Deallocate memory for the arrays
+    delete[] numbers;
+    delete[] primes;
+    delete[] nonPrimes;
+
+    return 0;
+}
